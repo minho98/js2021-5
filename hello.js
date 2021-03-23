66ea8f4a389d006f6feda5c315c9763667625637
user kim <201740204@daelim.ac.kr>
2021-03-23 16:49:32 +0900

    0

--- hello.js
+++ hello.js
@@ -1,8 +1,32 @@
-console.log("hello world...!");
+let date = new Date();
+h = date.getHours();
+
+console.log(h < 3 || h>8);
+console.log(h >= 3 && h <= 8 );
 
 
-/********************************
-// console.log("hello world...!");
-// console.log("hello world...!");
-// console.log("hello world...!");
- *********************************/
+let type = typeof(date.getHours()); 
+console.log(type);
+
+const con1 = "상수선엉";
+console.log(con1);
+
+let input = 31;
+
+if ( input % 2 == 0 ) {
+    console.log("짝수!");
+}
+   console.log("홀수!");
+//
+   if ( date.getHours() < 12 ) {
+    console.log("오전!");
+}
+   console.log("오후!");
+   // Date()
+   console.log(date.getFullYear());
+   console.log(date.getMonth());
+   console.log(date.getDay());
+   console.log(date.getTime());
+   console.log(date.getMinutes());
+   console.log(date.getSeconds());
+   console.log(date.getDate());
