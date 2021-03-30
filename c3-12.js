68d88b9cbf410416fe400fe4fd17186d09800303
user kim <201740204@daelim.ac.kr>
2021-03-30 16:50:02 +0900

    dsa

---
+++ c3-12.js
@@ -0,0 +1,15 @@
+let number = 31;
+console.log(number % 2 == 0?"짝수":"홀수");
+
+let test;
+test = test ? test : "초기화 해 주세요.";
+console.log(test);
+
+number = number ? number : "초기화 해 주세요.";
+console.log(number);
+//
+test = test ||"초기화 해 주세요.";
+console.log(test);
+
+number = number || "초기화 해 주세요.";
+console.log(number);
