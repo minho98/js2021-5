845613edac2fa5ee7f2530e244f0c7e0023ce595
minho <mh10998@naver.com>
2021-05-04 16:57:16 +0900

    ㅇ

--- README.md
+++ README.md
@@ -1,6 +1,23 @@
 # 김민호 [201740204]
 
 ## [5월4일]
+* toFixed이용해서 소수점자리 까지 출력하기 : toFixed() 안에 숫자 값에 따라서 소수점몇번째 까지 출력할껀지가 정해진다.<br>
+let foo = 25.123;<br>
+
+console.log(foo.toFixed(1));  값 : 25.1<br>
+console.log(foo.toFixed(5));  값 : 25.12300<br>
+
+* indexOf 이용하여 출력하기 :indexOf() 안에 값에 따라 조건문을 사용하여 조건을 충족 불충족 할경우 닶을 얻을수있다.<br>
+let foo = "좋은 아침입니다."<br>
+
+if(foo.indexOf('아침') >= 0){<br>
+    console.log("굿모닝.");<br>
+}<br>
+else {<br>
+    console.log("굿나잇");<br>
+}<br>
+
+* strig 객체들마다 출력하기 : strig 객체들은 원본 데이터를 건들지않는다<br>
 let foo = "abcdefg";<br>
 let bar = foo.toUpperCase();<br>
 let foobar = bar.toLowerCase();<br>
@@ -8,11 +25,11 @@ console.log(foo);<br>
 console.log(bar);<br>
 console.log(foobar);<br>
 
-foo : 소문자로  출력이된다
-bar : 대문자로  출력이된다
-foobar : 다시 소문자로 출력이 된다
+foo값 : 소문자로  출력이된다<br>
+bar값 : 대문자로  출력이된다<br>
+foobar값 : 다시 소문자로 출력이 된다<br>
 
-@ strig 객체들은 원본 데이터를 건들지않는다
+
 
 ## [4월27일]
 
--- ch07.js
+++ ch07.js
@@ -1,12 +1,23 @@
+//예제 7-3
+// let foo = "좋은 아침입니다."
+
+// if(foo.indexOf('아침') >= 0){
+//     console.log("굿모닝.");
+// }
+// else {
+//     console.log("굿나잇");
+// }
+
+
+
+
 //코드 7-12
-let foo = "abcdefg";
-let bar = foo.toUpperCase();
-let foobar = bar.toLowerCase();
-console.log(foo);
-console.log(bar);
-console.log(foobar);
-
-
+// let foo = "abcdefg";
+// let bar = foo.toUpperCase();
+// let foobar = bar.toLowerCase();
+// console.log(foo);
+// console.log(bar);
+// console.log(foobar);
 
 //예제 7-2
 // let foo = Number.MAX_VALUE;
@@ -16,12 +27,12 @@ console.log(foobar);
 // console.log(bar);
 
 //코드 7-8
-// function Construtor(){}
-// Construtor.foo = 273;
-// Construtor.bar = function(){};
+function Construtor(){}
+Construtor.foo = 273;
+Construtor.bar = function(){};
 
-// console.log(Construtor.foo);
-// console.log(Construtor.bar);
+console.log(Construtor.foo);
+console.log(Construtor.bar);
 
 //예제 7-1
 // let foo = 25.123;
